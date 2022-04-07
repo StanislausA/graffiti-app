906b0ca813ecb42949282610bd7a55a4693dc3ea
StanislausA <anthonystanislaus1@gmail.com>
2022-04-06 23:44:08 -0400

    post re-render still broken

--- client/App.js
+++ client/App.js
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './styles/app.css'
+import './styles/app.css';
 import Snippet from './components/Snippet';
 import { testData } from '../database/mockstore';
 
 const App = () => {
-
-  const [ data, setData ] = useState(null);
-  const [ text, setText ] = useState('');
+  const [data, setData] = useState(null);
+  const [text, setText] = useState('');
 
   useEffect(() => {
-    axios.get('/snippets')
+    axios
+      .get('/snippets')
       .then(({ data }) => {
-        const { snippets } = data;
-        setData(snippets);
-        console.log(snippets);
+        setData(data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
 
-  const stateReady = data
-    ? data.map((doc, idx) =>
-    <Snippet
-      key={`doc${ idx * 99 }data`}
-      content={doc.content}
-      date={doc.date.seconds}
-    />)
-    : <h1>Loading...</h1>;
+  const stateReady = data ? (
+    data.map((doc, idx) => (
+      <Snippet
+        key={`doc${idx * 99}data`}
+        content={doc.content}
+        date={doc.date.seconds}
+      />
+    ))
+  ) : (
+    <h1>Loading...</h1>
+  );
 
   const createSnippet = (event) => {
     event.preventDefault();
@@ -36,51 +37,52 @@ const App = () => {
       // display error to user
       return;
     }
-    axios.post('/snippets', { snippet })
-      .then(response => {
+    axios
+      .post('/snippets', { snippet })
+      .then((response) => {
         console.log(response);
         setText('');
-        // const updatedState = { snippet, ...data };
-        // setData(updatedState);
+        const updatedState = { snippet, ...data };
+        setData(updatedState);
       })
-      .catch(error => console.log(error));
-  }
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <div id='app'>
-     <header>
-       <h1>Graffiti-App</h1>
-       <div className='create-snippet'>
-        <textarea
-          className='snippet-textarea'
-          placeholder='write on the wall...'
-          maxLength='107'
-          cols='70'
-          rows='3'
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button
-          className='snippet-textarea-button'
-          type='button'
-          onClick={(e) => createSnippet(e)}
-        >
-          save
-        </button>
-       </div>
-     </header>
-     <nav>
-      <div className='nav-links'>
-        <a href='#'>About</a>
-        <a href='#'>Roadmap</a>
+    <div id="app">
+      <div className="header-nav">
+        <header>
+          <h1>Graffiti-App</h1>
+          <div className="create-snippet">
+            <textarea
+              className="snippet-textarea"
+              placeholder="write on the wall..."
+              maxLength="107"
+              cols="70"
+              rows="3"
+              onChange={(e) => setText(e.target.value)}
+            />
+            <button
+              className="snippet-textarea-button"
+              type="button"
+              onClick={(e) => createSnippet(e)}
+            >
+              save
+            </button>
+          </div>
+        </header>
+        <nav>
+          <div className="nav-links">
+            <a href="#">About</a>
+            <a href="#">Roadmap</a>
+          </div>
+        </nav>
       </div>
-     </nav>
-     <main>
-      <div className='snippet-cards'>
-         { stateReady }
-      </div>
-     </main>
+      <main>
+        <div className="snippet-cards">{stateReady}</div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
